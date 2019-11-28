5cf4066500cf6491c246473fbeb70ed9978bdcd2
uitspitss <3781626+uitspitss@users.noreply.github.com>
2019-11-29 07:11:03 +0900

    Update index page

--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -4,17 +4,23 @@ import styled from "@emotion/styled";
 import App from "../components/App";
 
 const rotation = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
   to {
     transform: rotate(360deg);
   }
 `;
 
-const Emoji = styled.div`
-  font-size: 500px;
+const Emoji = styled.p`
   animation: ${rotation} 5s linear infinite;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+  font-size: 300px;
+  width: 300px;
+  height: 300px;
+  line-height: 1;
 `;
 
 export default () => (

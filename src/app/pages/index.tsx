6bcaf606e042cfcd5f13096f93cba25cdc6acd42
uitspitss <3781626+uitspitss@users.noreply.github.com>
2019-11-28 21:30:27 +0900

    Update

--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -1,7 +1,24 @@
-import App from '../components/App'
+import { keyframes } from "@emotion/core";
+import styled from "@emotion/styled";
+
+import App from "../components/App";
+
+const rotation = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Emoji = styled.div`
+  font-size: 500px;
+  animation: ${rotation} 5s linear infinite;
+`;
 
 export default () => (
   <App>
-    <p>Index Page</p>
+    <Emoji>🚀</Emoji>
   </App>
-)
+);
